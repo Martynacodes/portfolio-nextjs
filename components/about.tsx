5d5b7d4412ee467a2896f61990bd89c4c37ef57e
Martyna Knell <martyna.salome@gmail.com>
2023-12-18 23:44:38 -0500

    Update content

--- components/about.tsx
+++ components/about.tsx
@@ -19,12 +19,12 @@ export default function About() {
     >
       <SectionHeading>About me</SectionHeading>
       <p className="mb-3">
-        After living my life emerged in classical music since the age of
-        ActiveSectionContextProvider, I decided to pursue my passion for
-        programming. I enrolled in a coding bootcamp and learned{" "}
+        After living my life emerged in classical music since the age of six, I
+        decided to pursue my passion for programming. I enrolled in a coding
+        bootcamp and learned{" "}
         <span className="font-medium">frontend web development</span>.{" "}
         <span className="italic">What fascinates me about programming</span> is
-        the never ending space to learn. I{" "}
+        the never-ending amount of knowledge to posses. I{" "}
         <span className="underline">love</span> learning the newest technologies
         and applying the knowledge in my projects. My core stack is{" "}
         <span className="font-medium">
